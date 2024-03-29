70b77cda5dcf5d23fba0ecab96431dd9e4fdd828
Ali Shaheen <ashaheen@coeus-solutions.de>
2024-03-30 00:23:48 +0100

    updated pip package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='workhub_client',
-    version='1.0.0',
+    version='1.0.5',
     packages=find_packages(),
     install_requires=[
         'requests'
